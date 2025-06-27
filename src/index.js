8fa44275ef3cb64f50299bea6816451a6090ebac
EstebanDiaz43 <esteban.demiljo@gmail.com>
2025-06-27 16:41:37 +0200

    Made use of HTMLElement

--- src/index.js
+++ src/index.js
@@ -3,18 +3,38 @@ import { createModal } from "./components/createModal.js";
 import { createGrid } from "./views/createGrid.js";
 import header from "./header.js";
 
+class PodcastPreview extends HTMLElement {
+  constructor() {
+    super();
+    this.shadow = this.attachShadow({ mode: "open" });
+  }
+
+  connectedCallback() {
+    this.render();
+  }
+
+  render() {
+    this.shadow.innerHTML = `
+   
+    <main id="podcastGrid" class="grid"></main>
+    `;
+  }
+}
+
+customElements.define("mypodcast-preview", PodcastPreview);
+
 /**
  * Initializes the podcast application.
  *
  * @principle SRP - Only responsible for application startup logic like event binding and rendering initial grid.
  */
 function init() {
+  document.body.insertAdjacentHTML("afterbegin", header());
   document
     .getElementById("closeModal")
     .addEventListener("click", createModal.close);
   const grid = createGrid();
   grid.render(podcasts);
-  document.body.insertAdjacentHTML("afterbegin", header());
 }
 
 init();
